2b8ec2ca0911ad758c75243b7cf729096819333f
Silas Baronda <silas.baronda@gmail.com>
2019-10-08 16:58:25 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Influx Playground
+
+> Includes Grafana for Visuals 😀
+
+```
+init.sh
+docker-compose up -d
+```
+
+http://localhost:3000
+
+## CLI
+
+```
+docker-compose exec influxdb influx
+```
---
+++ init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker network create monitoring
+docker volume create grafana-volume
+docker volume create influxdb-volume
+
+docker run --rm \
+  -e INFLUXDB_DB=db -e INFLUXDB_ADMIN_ENABLED=true \
+  -e INFLUXDB_ADMIN_USER=admin \
+  -e INFLUXDB_ADMIN_PASSWORD=supersecretpassword \
+  -e INFLUXDB_USER=telegraf -e INFLUXDB_USER_PASSWORD=secretpassword \
+  -v influxdb-volume:/var/lib/influxdb \
+  influxdb /init-influxdb.sh
